c060b7d8ae2b68407306d0803efba2e9c3ef6931
Will Weekes <williamweekes02@gmail.com>
2023-04-05 21:25:29 +0100

    Add features.js - for changing CSS e.c.t.

---
+++ features.js
@@ -0,0 +1,14 @@
+
+function toggleState (object) {
+    // var item = document.getElementById(object).style.display = "state";
+    var item = document.getElementById(object);
+    item.style.display = "none";
+    }
+
+
+    function changeElement(id) {
+    var el = document.getElementById(id);
+    el.style.color = "red";
+    el.style.fontSize = "15px";
+    el.style.backgroundColor = "#FFFFFF";
+  }
